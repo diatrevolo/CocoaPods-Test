6795b22515f518ffbef23cbf8907f7c18e750540
Roberto Osorio-Goenaga <rogoenaga@paperlesspost.com>
2017-06-08 16:26:50 -0400

    One solution

---
+++ Pods/TwilioChatClient/TwilioChatClient.h
@@ -0,0 +1,19 @@
+//
+//  TwilioChatClient.h
+//  TwilioChatClient
+//
+//  Created by Roberto Osorio-Goenaga on 6/8/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TwilioChatClient.
+FOUNDATION_EXPORT double TwilioChatClientVersionNumber;
+
+//! Project version string for TwilioChatClient.
+FOUNDATION_EXPORT const unsigned char TwilioChatClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwilioChatClient/PublicHeader.h>
+
+
